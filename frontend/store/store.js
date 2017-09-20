44e88890c4dec5cc898ff11fe53d8badf79415a6
Michael <mzhu105@gmail.com>
2017-09-20 16:19:16 -0700

    Remove redux extension

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,15 +1,10 @@
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import logger from "redux-logger";
-import { composeWithDevTools } from "redux-devtools-extension";
 
 import RootReducer from "../reducers/root_reducer";
 
 const configureStore = (preloadedState = {}) =>
-  createStore(
-    RootReducer,
-    preloadedState,
-    composeWithDevTools(applyMiddleware(thunk, logger))
-  );
+  createStore(RootReducer, preloadedState, applyMiddleware(thunk, logger));
 
 export default configureStore;
