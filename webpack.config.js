3f193a689059655378df4e88d3455a88d36b779c
Michael <mzhu105@gmail.com>
2017-09-28 18:38:59 -0700

    Move redux-logger out of production, add UglifyJsPlugin and use DefinePlugin to set process.env.NODE_ENV to 'production'

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,47 @@
 var path = require("path");
+var webpack = require("webpack");
+
+var plugins = [];
+var devPlugins = [];
+
+var prodPlugins = [
+  new webpack.DefinePlugin({
+    "process.env": {
+      NODE_ENV: JSON.stringify("production")
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: true
+    }
+  })
+];
+
+plugins = plugins.concat(
+  process.env.NODE_ENV === "production" ? prodPlugins : devPlugins
+);
 
 module.exports = {
   context: __dirname,
   entry: "./frontend/zoomm.jsx",
   output: {
-    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    path: path.resolve(__dirname, "app", "assets", "javascripts"),
     filename: "bundle.js"
   },
+  plugins: plugins,
   module: {
     loaders: [
       {
         test: [/\.jsx?$/, /\.js?$/],
         exclude: /node_modules/,
-        loader: 'babel-loader',
+        loader: "babel-loader",
         query: {
-          presets: ['es2015', 'react']
+          presets: ["es2015", "react"]
         }
       }
     ]
   },
-  devtool: 'source-maps',
+  devtool: "source-maps",
   resolve: {
     extensions: [".js", ".jsx", "*"]
   }
