22787e554a7c1083ebfc7e604057299122f439fd
Michael <mzhu105@gmail.com>
2017-12-09 00:15:27 -0800

    Revamp splash page

--- frontend/components/splash/splash_player.jsx
+++ frontend/components/splash/splash_player.jsx
@@ -3,15 +3,16 @@ import {Link} from "react-router-dom";
 
 const SplashPlayer = () => {
   return (
-    <div>
+    <div className="splash-page">
       <div className="splash-gif">
-        <img src="https://i.pinimg.com/originals/1d/f7/81/1df78147c4a8f5f19867a1a0cd0ea3c1.gif" />
+        <img src="http://res.cloudinary.com/foolishhunger/image/upload/v1512805477/splash1_ykuvzb.jpg" />
       </div>
       <div className="homepage-message">
-        <h1>Zoomm is a collection of best moments in life.</h1>
-        <h1>
-          <Link className="join-us" to="/signup"> &nbsp;Join us </Link>&nbsp;to see the best moments shared by the best photographers.
-        </h1>
+        <h1>Find your inspiration.</h1>
+        <h3>
+          Join the Zoomm community, home to tens of billions of photos and 2 millions groups.
+        </h3>
+        <Link className="join-us" to="/signup">Sign Up</Link>
       </div>
 
     </div>
@@ -19,3 +20,4 @@ const SplashPlayer = () => {
 };
 
 export default SplashPlayer;
+// <img src="https://i.pinimg.com/originals/1d/f7/81/1df78147c4a8f5f19867a1a0cd0ea3c1.gif" />
