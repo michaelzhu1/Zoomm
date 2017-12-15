27f0b2304276f1f58f3cfb9f7454d94d8a540c42
Michael <mzhu105@gmail.com>
2017-12-14 20:36:03 -0800

    Add photo time stamp

--- frontend/components/discover/discover_index_items.jsx
+++ frontend/components/discover/discover_index_items.jsx
@@ -12,8 +12,13 @@ class DiscoverIndexItems extends React.Component {
         <div className="photo-index-item-desc">
           <Link to={`/user/${this.props.photo.author_id}`}>
             <img src={this.props.photo.owner_avatar} className="user-avatar" />
-            <h4 className="photo-own-username">{this.props.photo.owner}</h4>
-          </Link>
+            </Link>
+            <div className="photo-attribution">
+              <Link to={`/user/${this.props.photo.author_id}`} className="photo-own-username">
+                {this.props.photo.owner}
+                </Link>
+                <span>{this.props.photo.age} ago</span>
+                </div>
         </div>
         <div>
           <img
