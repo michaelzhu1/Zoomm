cbf85cd1c05fcd51d553c99fd0465f55c665b046
Michael <mzhu105@gmail.com>
2017-12-14 17:02:51 -0800

    Add photo owner profile pic to photo index

--- frontend/components/discover/discover_index_items.jsx
+++ frontend/components/discover/discover_index_items.jsx
@@ -10,14 +10,15 @@ class DiscoverIndexItems extends React.Component {
     return (
       <div className="photo-index-item">
         <div className="photo-index-item-desc">
-          <Link to={`/user/${this.props.photo.author_id}`} className="photo-own-username">{this.props.photo.owner}</Link>
+          <img src={this.props.photo.owner_avatar} className="user-avatar"></img>
+          <Link to={`/user/${this.props.photo.author_id}`} className="photo-own-username">{this.props.photo.owner}
+          </Link>
         </div>
         <div>
           <img src={this.props.photo.photo_url} onClick={this.props.openPhoto(this.props.photo)}/>
           <div className="hidden-photo-info">
             <div className="message">
-              "{this.props.photo.photo_title}"&nbsp;
-              {this.props.photo.age} ago
+              {this.props.photo.photo_title}
             </div>
           </div>
         </div>
--- frontend/components/feed/feed_index_items.jsx
+++ frontend/components/feed/feed_index_items.jsx
@@ -10,14 +10,15 @@ class FeedIndexItems extends React.Component {
     return (
       <div className="photo-index-item">
         <div className="photo-index-item-desc">
-          <Link to={`/user/${this.props.photo.author_id}`} className="photo-own-username">{this.props.photo.owner}</Link>
+          <Link to={`/user/${this.props.photo.author_id}`} className="photo-own-username">{this.props.photo.owner}</Link>&nbsp;
+          {this.props.photo.age} ago
         </div>
         <div>
+
           <img src={this.props.photo.photo_url} onClick={this.props.openPhoto(this.props.photo)} />
           <div className="hidden-photo-info">
             <div className="message">
-              "{this.props.photo.photo_title}"&nbsp;
-              {this.props.photo.age} ago
+              <p>{this.props.photo.photo_title}</p>
             </div>
           </div>
         </div>
