ce687934a010975fe0b8304b45a623b78e5edb03
Michael <mzhu105@gmail.com>
2017-12-14 18:22:35 -0800

    Fix feed page profile pic

--- frontend/components/feed/feed_index_items.jsx
+++ frontend/components/feed/feed_index_items.jsx
@@ -10,15 +10,15 @@ class FeedIndexItems extends React.Component {
     return (
       <div className="photo-index-item">
         <div className="photo-index-item-desc">
-          <Link to={`/user/${this.props.photo.author_id}`} className="photo-own-username">{this.props.photo.owner}</Link>&nbsp;
-          {this.props.photo.age} ago
+          <Link to={`/user/${this.props.photo.author_id}`}><img src={this.props.photo.owner_avatar} className="user-avatar"></img>
+          <h4 className="photo-own-username">{this.props.photo.owner}</h4>
+          </Link>
         </div>
         <div>
-
           <img src={this.props.photo.photo_url} onClick={this.props.openPhoto(this.props.photo)} />
           <div className="hidden-photo-info">
             <div className="message">
-              <p>{this.props.photo.photo_title}</p>
+              {this.props.photo.photo_title}
             </div>
           </div>
         </div>
