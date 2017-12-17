04add69fc2f949f6441d5689de34c71beb1a08ea
Michael <mzhu105@gmail.com>
2017-12-16 18:33:27 -0800

    Add routes for comments

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     resources :users, only: [:create, :show, :update]
     resource :session, only: [ :create, :destroy, :show ]
-    resources :photos, only: [:create, :index, :show, :destroy, :update]
+    resources :photos, only: [:create, :index, :show, :destroy, :update] do
+      resources :comments, only: [:index, :create]
+    end
+    resources :comments, only: [:show, :update, :destroy]
     resources :follows, only: [:create, :destroy]
     get '/feed', to: 'photos#index_feed'
   end
