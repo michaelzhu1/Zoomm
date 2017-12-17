2cb6f282de991200ee3873d8e79413519a5ed456
Michael <mzhu105@gmail.com>
2017-12-16 18:58:31 -0800

    Add create for comments

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -3,7 +3,32 @@ class Api::CommentsController < ApplicationController
     @comments = Comment.where(photo_id: params[:photo_id]).includes(:author)
   end
 
+  def create
+    @comment = current_user.comments.new(comment_params)
+    @comment.photo_id = params[:photo_id]
+
+    if @comment.save
+      render :show
+    else
+      render json: @comment.errors.full_messages, status: 402
+    end
+  end
+
+  def show
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+
   private
+
   def comments_params
     params.require(:comment).permit(:body)
   end
