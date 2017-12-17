7656d6ae0364c42b58578a884bcad660dea96544
Michael <mzhu105@gmail.com>
2017-12-16 19:10:22 -0800

    Add Comment post ajax

--- frontend/util/comment_api_util.js
+++ frontend/util/comment_api_util.js
@@ -1,5 +1,14 @@
 export const getPhotoComments = (photoId) => (
   $.ajax({
-    url: `api/photos/${photoId}/comments`
+    url: `api/photos/${photoId}/comments`,
+    method: 'GET'
+  })
+);
+
+export const postComment = (comment, photoId) => (
+  $.ajax({
+    url:`api/photos/${photoId}/comments`,
+    method: 'POST',
+    data: { comment }
   })
 );
