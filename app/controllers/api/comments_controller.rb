b5a68294e1be9f0b79e9b7d95f0addba13f4406c
Michael <mzhu105@gmail.com>
2017-12-16 18:56:09 -0800

    Add index for comments

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,2 +1,10 @@
 class Api::CommentsController < ApplicationController
+  def index
+    @comments = Comment.where(photo_id: params[:photo_id]).includes(:author)
+  end
+
+  private
+  def comments_params
+    params.require(:comment).permit(:body)
+  end
 end
